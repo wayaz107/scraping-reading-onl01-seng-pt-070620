a7f6a007032335e90a554c8d43f5d66be79c24f0
Warda Ayaz <wayaz107@gmail.com>
2020-08-10 19:28:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,8 +3,7 @@ require 'open-uri'
 
 
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN").text
-
-
-puts doc.css(".headline-26OIBN")
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
