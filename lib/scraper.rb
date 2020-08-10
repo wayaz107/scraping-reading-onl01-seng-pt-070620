be523055f095b98f87aba91f6fd9eff6b83ec4d9
Warda Ayaz <wayaz107@gmail.com>
2020-08-10 19:33:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,10 +4,5 @@ require 'open-uri'
 
 
 html = open("https://flatironschool.com/")
-doc = Nokogiri::HTML(html)
 
-courses =doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
-courses.each do|course|
-  puts course.text.strip
-end 
